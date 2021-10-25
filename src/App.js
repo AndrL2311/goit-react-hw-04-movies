ab6b83c3ebc359dff4a6b4174b02d432f44452a0
Andrii Lypovetskyi <li_andr@ukr.net>
2021-10-25 23:20:01 +0300

    homePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-// import { Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import AppBar from "./components/AppBar/AppBar";
+import HomePage from "./vievs/HomePage";
+import MoviesPage from "./vievs/MoviesPage";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <AppBar />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/movies">
+          <MoviesPage />
+        </Route>
+        <Route>
+          <HomePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
