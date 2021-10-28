55a8273353b8cde928a02018c42a60ed80cf2c6f
Andrii Lypovetskyi <li_andr@ukr.net>
2021-10-28 09:34:56 +0300

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -22,7 +22,8 @@ function Cast({ movieId }) {
               "https://www.themoviedb.org/t/p/w300" + actor.profile_path;
           } else {
             actorImgUrl =
-              "https://upload.wikimedia.org/wikipedia/commons/c/c2/No_image_poster.png";
+              // "https://upload.wikimedia.org/wikipedia/commons/c/c2/No_image_poster.png";
+              "https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg";
           }
 
           return (
